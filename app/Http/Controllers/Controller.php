17f91b4ef663848d5322a8169461fd931d48ceb3
idril150 <ebibari1@gmail.com>
2022-11-08 10:56:26 -0600

    comentarios xd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+//hola que onda jorge?? 
+//como adnamios?
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
